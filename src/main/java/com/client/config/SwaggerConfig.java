6e685b15baa03184a77e357dfe11df7c0ef2c319
Sandeep Puvvada <SPuvvada@vitechinc.com>
2023-07-13 06:09:24 -0400

    swagger issue fixed

--- src/main/java/com/client/config/SwaggerConfig.java
+++ src/main/java/com/client/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-/*package com.client.config;
+package com.client.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,4 +21,3 @@ public class SwaggerConfig {
 													  .build();
 	}
 }
-*/
